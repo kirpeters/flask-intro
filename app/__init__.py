e0ec7223aded417631c781818c5b901528ecdc19
kirpeters <kpeters@waimea.school.nz>
2025-05-14 11:56:55 +1200

    s

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template
+from random import randint
+
 app = Flask(__name__) 
 
 @app.get("/")
@@ -9,3 +11,13 @@ def home():
 @app.get("/test/")
 def test():
     return render_template("pages/test.jinja")
+
+@app.get("/random/")
+def random():
+    randNum = randint(1, 1000)
+    return render_template("pages/random.jinja", number=randNum)
+
+@app.errorhandler
+def errorHandler():
+    return render_template("pages/404.jinja")
+
